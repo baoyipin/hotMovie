118bdfb42ab9a3dc44aa0a671be055fc87e041e8
baoyipin <baoyipin@126.com>
2018-04-06 18:31:09 +0800

    block

--- hotmovie/src/container/home/Block/MovieBlock.js
+++ hotmovie/src/container/home/Block/MovieBlock.js
@@ -4,23 +4,6 @@ import './MovieBlock.less'
 
 export default class MovieBlock extends Component {
 
-    render() {
-        let {actors, commonSpecial, id, img, dN, movieType, r, rd, tCn} = this.props.movie;
-        return (
-            <Link className='movie-item-wrap' to={`/detail/${id}`}>
-                <img src={img} alt=""/>
-                <div className='movie-item-info'>
-                    <h2>{tCn}
-                        <span className='movie-score'>{r}</span>
-                    </h2>
-                    <p className='movie-des'>{commonSpecial}</p>
-                    <p>导演：{dN}</p>
-                    <p>主演：{actors}</p>
-                    <p>类型：{movieType}</p>
-                    <p>上映时间：{rd}</p>
-                </div>
-            </Link>
-        )
-    }
+
 }
 
