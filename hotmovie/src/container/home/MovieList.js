f3c91444cb412e60583ceecef8407c96043864a5
baoyipin <baoyipin@126.com>
2018-04-06 16:50:23 +0800

    commit

--- hotmovie/src/container/home/MovieList.js
+++ hotmovie/src/container/home/MovieList.js
@@ -1,16 +1,29 @@
 import React from "react";
-
+import "./movieList.less";
+import {Link} from "react-router-dom";
 export default class MovieList extends React.Component{
 
     render(){
-
         return (
             <div className="listBox">
+                <p>热门电影</p>
                 {this.props.movies.map((item,index)=>(
-                    <div className="list" key={index}>
-                        <img src={movie.img}/>
-                    </div>
-                    
+                    <Link className="list" key={index} to={{pathname:`/detail/${item.id}`,state:item}}>
+                        <div className="img">
+                            <img src={item.img}/>
+                        </div>
+                        <div className="info">
+                            <p className="name">{item.tCn}</p>
+                            <p className="des">{item.commonSpecial}</p>
+                            <p className="detail">
+                                <li>导演：{item.dN}</li>
+                                <li>主演：{item.actors}</li>
+                                <li>类型：{item.movieType}</li>
+                                <li>上映时间：{item.rd}</li>
+                            </p>
+                        </div>
+                        <div className="score">{item.r}</div>
+                    </Link>
                 ))}
             </div>
         )
